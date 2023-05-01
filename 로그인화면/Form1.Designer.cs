92704ba21d3899b008fa0c6c2069f63b2bfe0a1c
simjaeyoun <spqjfgogo@gmail.com>
2023-05-01 12:41:09 +0900

    test

---
+++ 로그인화면/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace 로그인화면
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPW = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(209, 145);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 25);
+            this.txtID.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(222, 241);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPW
+            // 
+            this.txtPW.Location = new System.Drawing.Point(209, 193);
+            this.txtPW.Name = "txtPW";
+            this.txtPW.Size = new System.Drawing.Size(100, 25);
+            this.txtPW.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PW";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 449);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPW);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtID);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPW;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 로그인화면/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace 로그인화면
+{
+    public partial class Form1 : Form
+    {
+        ChromeOptions options;
+        string loginUrl;
+        ChromeDriver driver;
+
+        string Stu_cat;
+        string Stu_major;
+        string Stu_num;
+        string Stu_name;
+        string Stu_state;
+        public Form1()
+        {
+            InitializeComponent();
+            options = new ChromeOptions();
+            options.AddArgument("window-size=1920x1080");
+            options.AddArgument("headless");
+            loginUrl = "https://klas.kw.ac.kr/usr/cmn/login/LoginForm.do?redirectUrl=/std/cmn/frame/Frame.do";
+            
+            driver = new ChromeDriver(options);
+            //driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(loginUrl);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+                driver.FindElement(By.Id("loginId")).SendKeys(txtID.Text);
+                driver.FindElement(By.Id("loginPwd")).SendKeys(txtPW.Text);
+
+                driver.FindElement(By.ClassName("btn")).Click();
+                try
+                {
+                    //Wait main page
+                    WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+                    wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(
+                        By.XPath("/html/body/header/div[1]/div/div[2]/a[1]")));
+
+                    // Click main menu button
+                    driver.FindElement(By.XPath("/html/body/header/div[1]/div/div[1]/button")).Click();
+
+                    // Wait main menu
+                    WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                    wait2.Until(ExpectedConditions.ElementExists(
+                        By.XPath("/html/body/header/div[2]/div/div/div[1]/ul/li[2]/ul/li[2]/a")));
+
+                    // change 수강/성적 조회 url
+                    var link = driver.FindElement(By.XPath("/html/body/header/div[2]/div/div/div[1]/ul/li[2]/ul/li[2]/a"));
+                    var onclickAttr = link.GetAttribute("onclick");
+                    var urlStart = onclickAttr.IndexOf("'") + 1;
+                    var urlEnd = onclickAttr.IndexOf("'", urlStart);
+                    var url = onclickAttr.Substring(urlStart, urlEnd - urlStart);
+
+                    // Execute the linkUrl() function to change the URL
+                    ((IJavaScriptExecutor)driver).ExecuteScript($"linkUrl('{url}')");
+
+                    // Save student info
+                    var category = driver.FindElement(By.XPath("/html/body/main/div/div/div/div[2]/div/table[1]/tbody/tr/td[1]"));
+                    var major = driver.FindElement(By.XPath("/html/body/main/div/div/div/div[2]/div/table[1]/tbody/tr/td[2]"));
+                    var studentNum = driver.FindElement(By.XPath("/html/body/main/div/div/div/div[2]/div/table[1]/tbody/tr/td[3]"));
+                    var studentName = driver.FindElement(By.XPath("/html/body/main/div/div/div/div[2]/div/table[1]/tbody/tr/td[4]"));
+                    var studentState = driver.FindElement(By.XPath("/html/body/main/div/div/div/div[2]/div/table[1]/tbody/tr/td[5]"));
+
+                    Stu_cat = category.Text;
+                    Stu_major = major.Text;
+                    Stu_name = studentName.Text;
+                    Stu_num = studentNum.Text;
+                    Stu_state = studentState.Text;
+
+                }
+                catch
+                {
+                    //Login falut
+                    var pwfalut = driver.FindElement(By.XPath("/html/body/div[4]/div[2]/div[1]"));
+                    string LoginFalut = pwfalut.Text;
+                    
+                    // to find a number of login falut
+                    Match match = Regex.Match(LoginFalut, @"\d+");
+
+                    if (match.Success) // if password is falut
+                        MessageBox.Show("개인 번호 또는 비밀번호가 일치하지 않습니다\n" + "로그인 실패 건수 " + match.Value + "회");
+                    else // if id is falut
+                        MessageBox.Show("개인 번호 또는 비밀번호가 일치하지 않습니다.");
+
+                    // Click 확인button
+                    driver.FindElement(By.XPath("/html/body/div[4]/div[2]/div[2]/div/button")).Click();
+                    
+                    // if a number of login falut is over 5, restrict
+                    if (match.Value == "5")
+                        MessageBox.Show("비밀번호 오류 5회이상으로 로그인을 제한합니다.");
+                    //clear the input
+                    driver.FindElement(By.Id("loginId")).Clear();
+                    driver.FindElement(By.Id("loginPwd")).Clear();
+            }
+
+            MessageBox.Show(Stu_cat + Stu_major + Stu_name);
+        }
+    }
+}
